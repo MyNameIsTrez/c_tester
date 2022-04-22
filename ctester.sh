0a27b1059d0870bed441104caf4cc1cf979a95ef
MyNameIsTrez <sbos@student.codam.nl>
2022-04-22 13:43:21 +0200

    Make ctester.sh also run the created ctester binary,

--- ctester.sh
+++ ctester.sh
@@ -2,9 +2,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-pwd
 rm -f "$SCRIPT_DIR/to_test"
 ln -s "$(pwd)/$1" "$SCRIPT_DIR/to_test"
 make -C "$SCRIPT_DIR"
-# "$SCRIPT_DIR/test_all"
+"$SCRIPT_DIR/ctester"
 rm -f "$SCRIPT_DIR/to_test"
